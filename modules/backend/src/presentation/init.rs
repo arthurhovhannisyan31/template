01391fbfcd6d92e118fba9a38299c2f2b2e7b134
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-07-12 22:14:31 +0400

    add integration tests for router

--- modules/backend/src/presentation/init.rs
+++ modules/backend/src/presentation/init.rs
@@ -1,27 +1,28 @@
-use crate::infrastructure::config::AppConfig;
-use crate::infrastructure::error::ServerError;
 use crate::presentation::{
   auth::get_auth_router, common::AuthState, health::get_health_router,
   protected::get_protected_router,
 };
+use crate::{
+  infrastructure::config::AppConfig, infrastructure::cors::build_cors_layer,
+  infrastructure::error::ServerError,
+};
 
-use crate::infrastructure::cors::build_cors_layer;
 use axum::Router;
 use std::sync::Arc;
 use tokio::net::TcpListener;
 use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 use tracing::info;
 
-pub async fn init_http_server(
+pub fn build_router(
   auth_state: Arc<AuthState>,
   app_config: &AppConfig,
-) -> Result<(), ServerError> {
+) -> Router {
   let merged_router = Router::new()
     .merge(get_auth_router(auth_state.clone()))
     .merge(get_health_router())
     .merge(get_protected_router(auth_state.clone()));
 
-  let root_router = Router::new()
+  Router::new()
     .nest("/api", merged_router)
     .layer(TraceLayer::new_for_http().on_request(
       |request: &axum::extract::Request<_>, _span: &tracing::Span| {
@@ -29,7 +30,14 @@ pub async fn init_http_server(
       },
     ))
     .layer(CompressionLayer::new())
-    .layer(build_cors_layer(app_config));
+    .layer(build_cors_layer(app_config))
+}
+
+pub async fn init_http_server(
+  auth_state: Arc<AuthState>,
+  app_config: &AppConfig,
+) -> Result<(), ServerError> {
+  let root_router = build_router(auth_state, app_config);
 
   let listener =
     TcpListener::bind((app_config.host.to_string(), app_config.http_port))
