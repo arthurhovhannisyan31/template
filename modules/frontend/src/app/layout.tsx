e793adf27fa9b094c9cebf72e6862da4eea80afa
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-07-17 19:37:39 +0400

    add tailwind css

--- modules/frontend/src/app/layout.tsx
+++ modules/frontend/src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 
 import "./globals.css";
+import { cn } from "app/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +29,15 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html
+      lang="en"
+      className={cn(
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
+    >
       <body>{children}</body>
     </html>
   );
