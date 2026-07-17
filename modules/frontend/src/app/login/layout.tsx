cb5353aa8aaa811dd4731ce9e4b2e0ced6f871b2
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-07-18 00:00:46 +0400

    move login and signup pages styling to layouts

--- modules/frontend/src/app/login/layout.tsx
+++ modules/frontend/src/app/login/layout.tsx
@@ -14,5 +14,11 @@ export default async function LoginLayout({ children }: PropsWithChildren) {
     theme,
   });
 
-  return <section>{children}</section>;
+  return (
+    <section>
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">{children}</div>
+      </div>
+    </section>
+  );
 }
--- modules/frontend/src/app/signup/layout.tsx
+++ modules/frontend/src/app/signup/layout.tsx
@@ -6,5 +6,11 @@ export const metadata: Metadata = {
 };
 
 export default async function SignupLayout({ children }: PropsWithChildren) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">{children}</div>
+      </div>
+    </section>
+  );
 }
