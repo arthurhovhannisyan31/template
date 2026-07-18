b6c33cabbb0e10df2345027ca8a5020621154a71
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-07-18 17:53:04 +0400

    add root page keys and paths

---
+++ modules/frontend/src/app/configs/routes/constants.ts
@@ -0,0 +1,11 @@
+export enum RootKeys {
+  Dashboard = "",
+  Login = "login",
+  Signup = "signup",
+}
+
+export enum RootPath {
+  Dashboard = RootKeys.Dashboard,
+  Login = RootKeys.Login,
+  Signup = RootKeys.Signup,
+}
